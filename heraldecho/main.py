c512421e3a5ea27275d1564f4dafda3ed2b8cd84
Creohex <paradise.source@gmail.com>
2024-03-08 15:06:52 +0300

    update main

--- heraldecho/main.py
+++ heraldecho/main.py
@@ -8,10 +8,15 @@ from datetime import datetime
 from models import Configuration
 
 
+# FIXME: temporary...
+def p(*args, **kwargs):
+    print(*args, **kwargs, flush=True)
+
+
 def handler(tag, hook, message):
     """Default message emitter."""
 
-    print(f"{str(datetime.now())}: Executing job '{tag}'")
+    p(f"{str(datetime.now())}: Executing job '{tag}'")
     try:
         requests.post(url=hook, params={}, json={"content": message})
     except Exception as e:
@@ -23,10 +28,10 @@ if __name__ == "__main__":
     Configuration().load()
     schedule.clear()
     for job in Configuration().jobs:
-        print(f"Scheduling job: {str(job)}, freq: {job.frequency}")
+        p(f"Scheduling job: {str(job)}, freq: {job.frequency}")
         schedule.every(job.frequency).hours.do(handler, job.tag, job.hook, job.message)
 
-    print("Running...")
+    p("Running...")
     while True:
         schedule.run_pending()
         time.sleep(1)
